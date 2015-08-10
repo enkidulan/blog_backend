e26dd8bd7aa421227ead9fc8e53058c3cd3bb570
wercker builder <enkidulan@gmail.com>
2015-08-10 00:20:41 +0000

    Prepare version v1.0.201508100020 [ci skip]

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = [
     ]
 
 setup(name='blog_backend',
-      version='v1.0.201508100316',
+      version='v1.0.201508100020',
       description='blog_backend',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
